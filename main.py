88fbc51cbcb4650d9ed1fd7b8009e9db941806a0
RomanRodionov <rodionovv22@gmail.com>
2025-01-23 15:29:39 +0300

    Add parameterized BVH (wip)

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ class BVHEncoder(nn.Module):
         self.bvh.load_scene(cfg.mesh_path)
         self.bvh.build_bvh(10)
         self.bvh.save_as_obj("bvh.obj")
+        print(self.bvh.get_bbox(0))
 
     def forward(self, x):
         orig = x[..., :3]
@@ -226,12 +227,13 @@ class HashGridLoRAEncoder(nn.Module):
         return x    
 
 class ParameterizedAABB(nn.Module):
+    masks = [torch.tensor([False, True] * 4, device="cuda"), torch.tensor([False, False, True, True] * 2, device="cuda"), torch.tensor([False] * 4 + [True] * 4, device="cuda")]
+
     def __init__(self, vmin, vmax, feature_size):
         super().__init__()
         self.vmin = vmin
         self.vmax = vmax
-        self.features = torch.nn.Parameter(torch.randn(8, feature_size))#(torch.arange(8 * 2, dtype=torch.float).reshape(8, 2))
-        self.masks = [torch.tensor([False, True] * 4, device="cuda"), torch.tensor([False, False, True, True] * 2, device="cuda"), torch.tensor([False] * 4 + [True] * 4, device="cuda")]
+        self.features = torch.nn.Parameter(torch.randn(8, feature_size))
 
     def intersect(self, ray_orig, ray_dir):
         inv_dir = 1 / (ray_dir + 1e-6)
@@ -254,7 +256,6 @@ class ParameterizedAABB(nn.Module):
     def bicubic(self, point):
         coeffs = torch.ones((point.shape[0], 8), device="cuda")
         for i in range(3):
-            #coeffs *= point[:, i] * self.masks[i] + (1 - point[:, i]) * ~self.masks[i]
             coeffs[:, self.masks[i]] *= point[:, i].unsqueeze(1)
             coeffs[:, ~self.masks[i]] *= (1 - point[:, i]).unsqueeze(1)
         return torch.matmul(coeffs, self.features)
@@ -268,6 +269,36 @@ class ParameterizedAABB(nn.Module):
         p1, p2 = (p1 - self.vmin) / (self.vmax - self.vmin), (p2 - self.vmin) / (self.vmax - self.vmin)
         #print(dir[0], p1[0], p2[0])
         return torch.concat((self.bicubic(p1), self.bicubic(p2)), dim=1), mask, t_near, t_far
+    
+class ParameterizedBVHEncoder(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.bvh = BVH()
+        self.bvh.load_scene(cfg.mesh_path)
+        self.bvh.build_bvh(10)
+        self.bvh.save_as_obj("bvh.obj")
+        self.bboxes = [ParameterizedAABB(*self.bvh.get_bbox(i), 16) for i in range(self.bvh.n_nodes())]
+
+    def forward(self, x):
+        orig = x[..., :3]
+        vec = x[..., 3:] - x[..., :3]
+        vec = vec / vec.norm(dim=-1, keepdim=True)
+
+        orig = orig.cpu().numpy()
+        vec = vec.cpu().numpy()
+        mask, leaf_indices, t1, t2 = self.bvh.intersect_leaves(orig, vec)
+        orig = torch.tensor(orig, device="cuda", dtype=torch.float32)
+        vec = torch.tensor(vec, device="cuda", dtype=torch.float32)
+        mask = torch.tensor(mask, device="cuda", dtype=torch.bool)
+        t1 = torch.tensor(t1, device="cuda", dtype=torch.float32)
+        t2 = torch.tensor(t2, device="cuda", dtype=torch.float32)
+
+        x = torch.stack([orig + vec * t1[:, None], orig + vec * t2[:, None]], dim=1)
+        x = x.reshape(x.shape[0], -1)
+        x = x / self.cfg.sphere_radius
+        return x, mask, t1[:, None], t2[:, None]
 
 class SinEncoder(nn.Module):
     def __init__(self, dim, factor=1):
@@ -653,12 +684,12 @@ def main(cfg):
     # encoder = HashGridEncoder(range=1, dim=3, log2_hashmap_size=22, finest_resolution=256)
     # encoder = HashGridLoRAEncoder(range=1, dim=3, log2_hashmap_size=18, finest_resolution=256, rank=2048) # rank = None
     # encoder = SinEncoder(8, 1)
-    #point_encoder = ParameterizedAABB(torch.tensor([-50, -50, -50], device="cuda"), torch.tensor([50, 50, 50], device="cuda"), 16)
+    point_encoder = ParameterizedAABB(torch.tensor([-50, -50, -50], device="cuda"), torch.tensor([50, 50, 50], device="cuda"), 16)
     #encoder = None
     # point_encoder = NPointEncoder(cfg, N=32, sphere_radius=cfg.sphere_radius)
-    point_encoder = BVHEncoder(cfg)
-    encoder = HashGridEncoder(range=1, dim=3, log2_hashmap_size=14, finest_resolution=256)
-    # encoder = None
+    #point_encoder = BVHEncoder(cfg)
+    #encoder = HashGridEncoder(range=1, dim=3, log2_hashmap_size=14, finest_resolution=256)
+    encoder = None
     net = MLPNet(128, 6, use_tcnn=True)
     # net = TransformerNet(24, 3, 32, use_tcnn=True, attn=False, norm=True)
     model = Model(point_encoder, encoder, net).cuda()
