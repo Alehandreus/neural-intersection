7dfeda3346f61ce3c38acc2c561e84c3760a7d75
Alexander <alexander.nikolaev@graphics.cs.msu.ru>
2025-05-14 20:44:07 +0300

    move blendorays to misc

--- main.py
+++ main.py
@@ -8,62 +8,7 @@ from myutils.ray import *
 from nbvh_model import *
 from trainer import Trainer
 
-from bvh import Mesh, CPUBuilder, GPUTraverser, GPURayGen
-
-
-def save_rays_blender(bvh_data, bvh, batch_size):
-    print("raygen...")
-
-    nodes_min, nodes_max = bvh_data.nodes_data()
-    nodes_min = torch.tensor(nodes_min, device='cuda')
-    nodes_max = torch.tensor(nodes_max, device='cuda')
-    nodes_extent = nodes_max - nodes_min
-
-    raygen = GPURayGen(bvh, batch_size)
-    ray_origins = torch.zeros((batch_size, 3), device="cuda", dtype=torch.float32)
-    ray_vectors = torch.zeros((batch_size, 3), device="cuda", dtype=torch.float32)
-    t1 = torch.zeros((batch_size,), device="cuda", dtype=torch.float32)
-    masks = torch.zeros((batch_size,), device="cuda", dtype=torch.bool)
-    bbox_idxs = torch.zeros((batch_size,), device="cuda", dtype=torch.uint32)
-    normals = torch.zeros((batch_size, 3), device="cuda", dtype=torch.float32)
-    bvh.grow_nbvh(12)
-    for i in range(8):
-        print(raygen.raygen(ray_origins, ray_vectors, masks, t1, bbox_idxs, normals))
-        print((t1 > 1.01).sum().item(), t1.max().item())
-
-    with open("rays.obj", 'w') as f:
-        vertex_index = 1
-        for (p1, p2, t, normal, mask, bbox_idx) in zip(ray_origins, ray_vectors, t1, normals, masks, bbox_idxs):
-            if not mask.item(): continue
-
-            if t < 1.01: continue
-
-            p3 = p1 + (p2 - p1) * t
-            p4 = p3 + normal * 0.1
-            p1 = p1.cpu().squeeze()
-            p2 = p2.cpu().squeeze()
-            p3 = p3.cpu().squeeze()
-            p4 = p4.cpu().squeeze()
-            
-            f.write(f'v {p1[0].item()} {p1[1].item()} {p1[2].item()}\n')
-            f.write(f'v {p2[0].item()} {p2[1].item()} {p2[2].item()}\n')            
-            f.write(f'v {p3[0].item()} {p3[1].item()} {p3[2].item()}\n')
-            f.write(f'v {p4[0].item()} {p4[1].item()} {p4[2].item()}\n')
-
-            print(f"==========================")
-            print(f"min: {nodes_min[bbox_idx]}")
-            print(f"max: {nodes_max[bbox_idx]}")
-            print(f"extent: {nodes_extent[bbox_idx]}")
-            print(f"bbox_idx: {bbox_idx}")
-            print(f"t: {t.item()}")
-            print(f"p1: {p1}")
-            print(f"p2: {p2}")
-            
-            # f.write(f'l {vertex_index} {vertex_index + 1}\n')
-            # f.write(f'l {vertex_index} {vertex_index + 2}\n')
-            # f.write(f'l {vertex_index + 2} {vertex_index + 1}\n')
-            f.write(f'l {vertex_index + 2} {vertex_index + 3}\n')
-            vertex_index += 4
+from bvh import Mesh, CPUBuilder, GPUTraverser
 
 
 @hydra.main(config_path="config", config_name="nbvh", version_base=None)
@@ -164,7 +109,6 @@ if __name__ == "__main__":
     try:
         torch.set_float32_matmul_precision("high")
         main()
-        # test_mem()
     except KeyboardInterrupt:
         print("Stopping...")
         exit()
